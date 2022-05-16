03e656e167573ca0291c4de91743b9efd758c1e8
miriamay <105532752+miriamay@users.noreply.github.com>
2022-05-16 22:00:43 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let is_running = false;
 let demo_button = document.getElementById("start_demo");
 let currentMovement = "1";
 
-console.log("v44");
+console.log("v46");
 
 const gainNode = new Tone.Gain(0).toDestination();
 const gainNode2 = new Tone.Gain(0).connect(gainNode);
@@ -221,3 +221,15 @@ demo_button.onclick = function (e) {
     is_running = true;
   }
 };
+
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState !== 'visible') {
+    demo_button.innerHTML = "START";
+    document.getElementById("circle").style.background = "green";
+    Lyre.stop();
+    Flute.stop();
+    Witches.stop();
+    Owl.stop();
+    is_running = false;
+  } 
+});
